6194c5182880d1c09736ffa4a1b4e5248546fe47
Eugen <Eugen@WWAEB23.aeb-software.de>
2015-10-22 14:22:30 +0200

    g

--- javaclient/src/main/java/com/aeb/elan9/gui/javaclient/MainApp2.java
+++ javaclient/src/main/java/com/aeb/elan9/gui/javaclient/MainApp2.java
@@ -4,35 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.web.HTMLEditor;
-import javafx.scene.web.WebView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainApp2 extends Application {
 
+	private ResourceBundle bundle;
 	private static final Logger log = LoggerFactory.getLogger(MainApp2.class);
 
 	public void start(Stage stage) throws Exception {
+		
 		String fxmlFile = "/fxml/MessageDialog.fxml";
 		log.debug("Loading FXML for main view from: {}", fxmlFile);
 		FXMLLoader loader = new FXMLLoader();
-		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-		log.debug("Showing JFX scene");
-		Scene scene = new Scene(rootNode, 400, 200);
-		scene.getStylesheets().add("/styles/styles.css");
 		
-		HTMLEditor editor = (HTMLEditor) scene.lookup("#htmlEditor");
-		stage.setTitle("MessageDialog");
+		bundle = ResourceBundle.getBundle("bundles.lang", new Locale("en","EN"));
+		loader.setResources(bundle);
+//		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+		VBox rootNode = (VBox) loader.load(getClass().getResourceAsStream(fxmlFile));
+		log.debug("Showing JFX scene");
+		Scene scene = new Scene(rootNode, 800, 600);
+		scene.getStylesheets().add("/styles/styles.css");
+
+		stage.setTitle(bundle.getString("dialog.title"));
 		stage.setScene(scene);
 		stage.show();
-
-		WebView webview = (WebView) editor.lookup("WebView");
-		GridPane.setHgrow(webview, Priority.ALWAYS);
-		GridPane.setVgrow(webview, Priority.ALWAYS);
 	}
 
 	public static void main(String[] args) throws Exception {
--- javaclient/src/main/java/com/aeb/elan9/gui/javaclient/MessageDialogController.java
+++ javaclient/src/main/java/com/aeb/elan9/gui/javaclient/MessageDialogController.java
@@ -1,33 +1,78 @@
 package com.aeb.elan9.gui.javaclient;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MessageDialogController {
+public class MessageDialogController implements Initializable {
 
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+	//	public static void main(String[] args) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
 
-    private static final Logger log = LoggerFactory.getLogger(MessageDialogController.class);
-    
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(MessageDialogController.class);
 
-    @FXML private TextField fromTextField;
-    @FXML private TextField toTextField;
-    @FXML private TextField subjectTextField;
-    @FXML private Button sendButton;
-    @FXML private Button cancelButton;
-    
-    public void exit(){
-    	System.out.println("closing");
-    }
-	
-	
+	@FXML
+	private TextField fromTextField;
+	@FXML
+	private TextField toTextField;
+	@FXML
+	private TextField subjectTextField;
+	@FXML
+	private Button sendButton;
+	@FXML
+	private Button cancelButton;
+	@FXML
+	private HTMLEditor htmlEditor;
+	@FXML
+	private Label subjectLabel;
+	@FXML
+	private Label fromLabel;
+	@FXML
+	private Label toLabel;
+
+	public void exit() {
+		System.out.println("closing");
+		System.exit(0);
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		log.debug("Initializing" + getClass().getName());
+		WebView webview = (WebView) htmlEditor.lookup("WebView");
+		GridPane.setHgrow(webview, Priority.ALWAYS);
+		GridPane.setVgrow(webview, Priority.ALWAYS);
+	}
+
+	public void openDoc() {
+		Scene scene = sendButton.getScene();
+		Window window = scene.getWindow();
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"),new FileChooser.ExtensionFilter("HTML", "*.html"));
+		fileChooser.setTitle("Open Resource File");
+		File file = fileChooser.showOpenDialog(window);
+		if(file != null){
+			htmlEditor.setHtmlText("Hallo");
+		}
+	}
+
 }
